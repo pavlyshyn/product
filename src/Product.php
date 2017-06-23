9c8d2ba218d4d2ad705ba4d2f866ffe793f0bfb6
Roma Pavlyshyn <pavlyshyn.roma@gmail.com>
2017-06-23 20:10:43 +0300

    Create Product.php
    Initial commit

---
+++ src/Product.php
@@ -0,0 +1,32 @@
+<?php
+namespace Pavlyshyn\Product;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Request;
+
+class Product extends Model {
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'products';
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'amount',
+        'discount',
+        'code',
+        'meta_title',
+        'meta_description',
+        'meta_keywords',
+        'is_publish'
+    ];
+}
