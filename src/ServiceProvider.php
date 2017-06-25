2d4f1708587553ddbcbf7a520608636a50953a76
Pavlyshyn Roman <pavlyshyn.roma@gmail.com>
2017-06-25 03:09:32 +0300

    translations init

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,13 +8,10 @@ class ServiceProvider extends LServiceProvider {
 
     public function boot() {
 
-        //Указываем что пакет должен опубликовать при установке
         $this->publishes([__DIR__ . '/../config/' => config_path() . "/"], 'config');
         $this->publishes([__DIR__ . '/../database/' => base_path("database")], 'database');
-        // Routing
-        if (!$this->app->routesAreCached()) {
-            include __DIR__ . '/routes.php';
-        }
+        $this->publishes([__DIR__ . '/../lang' => resource_path('lang/vendor/product')]);
+        $this->loadRoutesFrom(__DIR__.'/routes.php');
     }
 
     public function register() {
